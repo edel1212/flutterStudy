a00a95bb6c9040d747c6a80eff5319c4bedb1caa
jeoungho.yoo <jeoungho.yoo@stans.co.kr>
2024-04-03 17:11:47 +0900

    Header UI 구현

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -15,8 +15,48 @@ class App extends StatelessWidget {
      * 
      * Flutter 자체가 Google에서 만들었기에 Material가 좀 더 최적화된 디자인이 많다
      */
-    return const MaterialApp(
-      home: Text('Hello world!!'),
-    );
+    return MaterialApp(
+        /**
+       * 👉 일반 적인 UI 구성은 Scaffold 구조로 되어있기에 FLutter에서도
+       *    Scaffold Class가 있다! 사용해주자
+       */
+        home: Scaffold(
+      backgroundColor: Color(0xFF181818),
+      // 패딩 적용
+      body: Padding(
+          // 패딩 적용 방식
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            children: [
+              Row(
+                // 👉 해당 내부 요소 정렬 기준 - 수평 기준
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    // 👉 해당 내부 요소 정렬 기준 - 수직 기준
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      const Text(
+                        "Hey Yoo",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 38,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      Text(
+                        "Welcome back",
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                          fontSize: 18,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          )),
+    ));
   }
 }
